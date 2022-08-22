7961ce30eaf63866a30727d38f7ae288deb24faa
Vincent Langat <kimutailangat033@gmail.com>
2022-08-22 18:42:50 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if @length1 == @length2 and @length2 == @length3
+      :equilateral
+    elsif @length1 == @length2 or @length1 == @length3 or @length2 == @length3
+      :isosceles
+    elsif @length1 != @length2 and @length1 != @length3 and @length2 != @length3 
+      :scalene
+    elsif @length1 == 0 or @length2 ==0 or @length3== 0
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+isosceles = Triangle.new(4,4,5)
